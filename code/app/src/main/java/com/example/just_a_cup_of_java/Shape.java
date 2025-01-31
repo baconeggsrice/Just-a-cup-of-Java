c14be054c888b417a86f1b7e0f6935aa331a0a4a
Heena Kamani <hkamani@ualberta.ca>
2025-01-30 21:00:40 -0700

    My color choice

--- code/app/src/main/java/com/example/just_a_cup_of_java/Shape.java
+++ code/app/src/main/java/com/example/just_a_cup_of_java/Shape.java
@@ -3,7 +3,7 @@ package com.example.just_a_cup_of_java;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    private String color = "Blue";
 
     public Shape(int x, int y) {
         this.x = x;
--- code/app/src/main/java/com/example/just_a_cup_of_java/Triangle.java
+++ code/app/src/main/java/com/example/just_a_cup_of_java/Triangle.java
@@ -4,4 +4,5 @@ import android.graphics.drawable.shapes.Shape;
 
 public class Triangle extends Shape {
     private float x1, y1, x2, y2, x3, y3;
+
 }
