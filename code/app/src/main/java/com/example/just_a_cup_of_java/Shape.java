6737dc48c2ad6e6f28a1e55bf8346537f916ad82
baconeggsrice <144288764+baconeggsrice@users.noreply.github.com>
2025-01-30 19:45:05 -0700

    Update Shape.java

--- code/app/src/main/java/com/example/just_a_cup_of_java/Shape.java
+++ code/app/src/main/java/com/example/just_a_cup_of_java/Shape.java
@@ -3,7 +3,7 @@ package com.example.just_a_cup_of_java;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y) {
         this.x = x;
